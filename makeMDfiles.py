060076b21e880232723ca76b5699085abd99146b
Mookie Kong <mookie@outlook.com>
2017-01-09 06:03:57 +0000

    initial

---
+++ makeMDfiles.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+import pprint
+
+# Code for series
+# ENT - Enterprise
+# TOS - The Original Series
+# TAS - The Animated Series
+# TNG - The Next Generation
+# DS9 - Deep Space Nine
+# VOY - Voyager
+series = sys.argv[1]
+season = '0' + sys.argv[2]
+filename = sys.argv[3]
+
+reader = csv.DictReader(open(filename, 'rb'))
+
+for episode in reader:
+  episodeNum = episode['num'].zfill(2)
+  episodeTitle = episode['title']
+  episodeFileTitle = episodeTitle.replace(" ", "-")
+  episodeInfo = series + '-S' + season + 'E' + episodeNum + '-' + episodeFileTitle
+  episodeFilename = 'YYYY-MM-DD-' + episodeInfo + '.md'
+  fileTitle = episodeTitle + ' (' + series + ' S' + season + 'E' + episodeNum + ')' + '\n'
+  episodeFile = open(episodeFilename,'w')
+  episodeFile.write(fileTitle)
+  episodeFile.write('=======================\n')
+  episodeFile.write('Month DD, 2016')
+  episodeFile.close()
